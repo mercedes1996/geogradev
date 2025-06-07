e5845729f433111c46868485a2d4353e29d62f98
Mercedes <aguirrecername@gmail.com>
2025-06-07 13:03:28 -0500

    update

--- geogradev/geogradev.py
+++ geogradev/geogradev.py
@@ -1,1 +1,11 @@
 """Main module."""
+
+import ipyleaflet
+
+
+class Map(ipyleaflet.Map):
+    """Custom Map class that extends ipyleaflet.Map."""
+
+    def __init__(self, **kwargs):
+        """Initialize the Map with custom settings."""
+        super().__init__(**kwargs)
