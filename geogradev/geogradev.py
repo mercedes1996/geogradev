974f5f8efd1879efe51035a2fcbc843f416e990e
Mercedes <aguirrecername@gmail.com>
2025-06-07 17:41:25 -0500

    update

--- geogradev/geogradev.py
+++ geogradev/geogradev.py
@@ -1,9 +1,8 @@
 """Main module."""
 
-import intro
+import ipyleaflet
 
 
-class Map(intro.Map):
-    def __init__(self, center=(0, 0), zoom=2, **kwargs):
-        super().__init__(location=center, zoom_start=zoom, **kwargs)
-        intro.LayerControl().add_to(self)
+class Map(ipyleaflet.Map):
+    def __init__(self, center=[20, 0], zoom=2**kwargs):
+        super().__init__(center=center, zoom=zoom, **kwargs)
