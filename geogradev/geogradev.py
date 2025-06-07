3bb0cf9074679868f2342ecec6593fada55150d7
Mercedes <aguirrecername@gmail.com>
2025-06-07 14:06:59 -0500

    update

--- geogradev/geogradev.py
+++ geogradev/geogradev.py
@@ -6,3 +6,12 @@ import ipyleaflet
 class Map(ipyleaflet.Map):
     def __init__(self, center=[20, 0], zoom=2, height="600px", **kwargs):
         super().__init__(center=center, zoom=zoom, **kwargs)
+
+
+# geogradev.py
+__all__ = ["Map"]
+
+
+class Map(ipyleaflet.Map):
+    def __init__(self, center=[20, 0], zoom=2, height="600px", **kwargs):
+        super().__init__(center=center, zoom=zoom, **kwargs)
